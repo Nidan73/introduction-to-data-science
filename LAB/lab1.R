d0b025712454543fbf3d140770e2bdb172d6c2a7
Nidan73 <111710686+Nidan73@users.noreply.github.com>
2025-03-04 10:19:56 +0600

    Add files via upload

---
+++ LAB/lab1.R
@@ -0,0 +1,43 @@
+1+1
+print("Hello world")
+
+2+3*4
+3^2
+sqrt(10)
+pi
+
+x <- -8
+y <- 5.5
+z <- 2
+
+x + y - z
+
+a <- "string"
+b<- "NIDAN"
+abs(x)
+
+sqrt(y)
+
+ceiling(y)
+
+floor(y)
+
+toupper(a)
+
+tolower(b)
+
+strsplit(b,split="")
+
+nchar(a)
+
+paste(a,b)
+paste0(a,b)
+
+if(x>y){
+  print("A is a bigger numb")
+}else if{
+  print("both are  equal numb")
+}else{
+  print("B is a bigger numb")
+  }
+
