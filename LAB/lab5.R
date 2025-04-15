44f7178ed7b8aa09acb19cb5565a67b518ef8887
Nidan73 <111710686+Nidan73@users.noreply.github.com>
2025-04-15 10:02:52 +0600

    Add files via upload

---
+++ LAB/lab5.R
@@ -0,0 +1,29 @@
+file_path <- "D:/iris.csv"
+has_header <- TRUE
+delimiter <- ","
+a <- read.csv(file = file_path, header = has_header, sep = delimiter)
+
+
+b <- read.csv("D:/iris.csv", header = TRUE, sep = ",")
+
+
+print(b)
+
+vars <- c("sepal.length","sepal.width","petal.length","petal.width")
+
+head(b[vars])
+
+summary(b[vars])
+
+b$variety <- factor(b$variety,
+                    levels = c("Setosa","Versicolor","Virginica"),
+                    labels=c(1,2,3))
+
+b$variety
+
+s<- b$sepal.length
+sd(s)
+
+
+is.na(b)
+colSums(is.na(b))
